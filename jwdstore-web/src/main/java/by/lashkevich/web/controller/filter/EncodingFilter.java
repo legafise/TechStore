e270d514fcf842604ce66e17f2b8492cccd7fba7
legafise <lashkevich1974@gmail.com>
2021-11-19 19:15:18 +0300

    Introduce web filters

---
+++ jwdstore-web/src/main/java/by/lashkevich/web/controller/filter/EncodingFilter.java
@@ -0,0 +1,40 @@
+package by.lashkevich.web.controller.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "EncodingFilter", initParams = {
+        @WebInitParam(name = "ENCODING", value = "UTF-8", description = "Encoding param")
+})
+public class EncodingFilter implements Filter {
+    private String encoding;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        encoding = filterConfig.getInitParameter("ENCODING");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        String requestEncoding = servletRequest.getCharacterEncoding();
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpServletResponse = (HttpServletResponse) servletResponse;
+
+
+        if (encoding != null && !encoding.equals(requestEncoding)) {
+            httpServletRequest.setCharacterEncoding(encoding);
+            httpServletResponse.setCharacterEncoding(encoding);
+        }
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    @Override
+    public void destroy() {
+        encoding = null;
+    }
+}
---
+++ jwdstore-web/src/main/java/by/lashkevich/web/controller/filter/PageRedirectSecurityFilter.java
@@ -0,0 +1,34 @@
+package by.lashkevich.web.controller.filter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(filterName = "PageRedirectSecurityFilter", initParams = {
+        @WebInitParam(name = "ERROR-PAGE-PATH", value = "/controller?command=error")
+})
+public class PageRedirectSecurityFilter implements Filter {
+    private static final String ERROR_PAGE_PATH_INIT_PARAMETER_NAME = "ERROR-PAGE-PATH";
+    private String errorPagePath;
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        errorPagePath = filterConfig.getInitParameter(ERROR_PAGE_PATH_INIT_PARAMETER_NAME);
+    }
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        httpResponse.sendRedirect(httpRequest.getContextPath() + errorPagePath);
+        chain.doFilter(request, response);
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
