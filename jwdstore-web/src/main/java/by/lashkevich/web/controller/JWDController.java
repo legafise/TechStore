2f04eeac6668d6bf630bd86c095ca6c007f25325
legafise <lashkevich1974@gmail.com>
2021-11-19 19:14:26 +0300

    Create controller

---
+++ jwdstore-web/src/main/java/by/lashkevich/web/controller/JWDController.java
@@ -0,0 +1,66 @@
+package by.lashkevich.web.controller;
+
+import by.lashkevich.logic.dao.pool.ConnectionPool;
+import by.lashkevich.logic.dao.pool.ConnectionPoolException;
+import by.lashkevich.web.controller.command.Command;
+import by.lashkevich.web.controller.command.CommandException;
+import by.lashkevich.web.controller.command.CommandFactory;
+import by.lashkevich.web.controller.command.CommandResult;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet("/controller")
+public class JWDController extends HttpServlet {
+    private static final Logger LOGGER = LogManager.getRootLogger();
+
+    @Override
+    public void init() {
+        try {
+            ConnectionPool.getInstance().initializeConnectionPool(3);
+        } catch (ConnectionPoolException e) {
+            LOGGER.error(e);
+        }
+    }
+
+    @Override
+    public void destroy() {
+        try {
+            ConnectionPool.getInstance().closeConnections();
+        } catch (ConnectionPoolException e) {
+            LOGGER.error(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        try {
+            Command command = CommandFactory.findCommandByRequest(req);
+            CommandResult commandResult = command.execute(req);
+
+            if (commandResult.getResponseType() == CommandResult.ResponseType.FORWARD) {
+                req.getRequestDispatcher(commandResult.getPage()).forward(req, resp);
+            } else {
+                resp.sendRedirect(commandResult.getPage());
+            }
+        } catch (ServletException | IOException | CommandException e) {
+            LOGGER.error(e);
+            resp.sendRedirect(req.getContextPath() + "/controller?command=error");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        doGet(req, resp);
+    }
+}
